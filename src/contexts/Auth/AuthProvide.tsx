56323430deffeb3edbfa41d8a91ecd0cfbe2853a
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-04-05 14:57:46 +0100

    configured authProvider

--- src/contexts/Auth/AuthProvide.tsx
+++ src/contexts/Auth/AuthProvide.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { useApi } from "../../hooks/useApi";
+import { User } from "../../types/User";
 import { AuthContext } from "./AuthContext";
 
+//authProvider does the authentication context, so you know if it did or not the login
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const api = useApi();
+
+  const signin = async (email: string, password: string) => {
+    const data = await api.signin(email, password);
+    if (data.user && data.token) {
+      setUser(data.user);
+      return true;
+    }
+    return false;
+  };
+
+  const signout = async () => {
+    api.logout();
+    setUser(null);
+  };
   return (
     <AuthContext.Provider value={{ user, signin, signout }}>
       {children}
